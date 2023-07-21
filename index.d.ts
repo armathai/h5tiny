da98031e69a2ff938d7f931724a735932aef5eb3
saqsun <sargsyan.sargis.89@gmail.com>
2023-07-21 17:47:50 +0400

    added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,225 @@
+declare module 'h5tiny/App' {
+  function noop(): void;
+  //# sourceMappingURL=App.d.ts.map
+}
+declare module 'h5tiny/App.d.ts' {
+  {"version":3,"file":"App.d.ts","sourceRoot":"src/","sources":["App.js"],"names":[],"mappings":"AAAA,8BAAyB"}
+}
+declare module 'h5tiny/base' {
+  export {};
+  //# sourceMappingURL=base.d.ts.map
+}
+declare module 'h5tiny/base.d.ts' {
+  {"version":3,"file":"base.d.ts","sourceRoot":"src/","sources":["base.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/global' {
+  //# sourceMappingURL=global.d.ts.map
+}
+declare module 'h5tiny/global.d.ts' {
+  {"version":3,"file":"global.d.ts","sourceRoot":"src/","sources":["global.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/index' {
+  export {};
+  //# sourceMappingURL=index.d.ts.map
+}
+declare module 'h5tiny/index.d.ts' {
+  {"version":3,"file":"index.d.ts","sourceRoot":"src/","sources":["index.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/math/Circle' {
+  //# sourceMappingURL=Circle.d.ts.map
+}
+declare module 'h5tiny/math/Circle.d.ts' {
+  {"version":3,"file":"Circle.d.ts","sourceRoot":"src/","sources":["math/Circle.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/math/Math' {
+  var degreeToRadiansFactor: number;
+  var radianToDegreesFactor: number;
+  //# sourceMappingURL=Math.d.ts.map
+}
+declare module 'h5tiny/math/Math.d.ts' {
+  {"version":3,"file":"Math.d.ts","sourceRoot":"src/","sources":["math/Math.js"],"names":[],"mappings":"AASA,0CAA0C;AAC1C,0CAA0C"}
+}
+declare module 'h5tiny/math/Matrix' {
+  //# sourceMappingURL=Matrix.d.ts.map
+}
+declare module 'h5tiny/math/Matrix.d.ts' {
+  {"version":3,"file":"Matrix.d.ts","sourceRoot":"src/","sources":["math/Matrix.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/math/Point' {
+  //# sourceMappingURL=Point.d.ts.map
+}
+declare module 'h5tiny/math/Point.d.ts' {
+  {"version":3,"file":"Point.d.ts","sourceRoot":"src/","sources":["math/Point.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/math/Polygon' {
+  //# sourceMappingURL=Polygon.d.ts.map
+}
+declare module 'h5tiny/math/Polygon.d.ts' {
+  {"version":3,"file":"Polygon.d.ts","sourceRoot":"src/","sources":["math/Polygon.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/math/Rectangle' {
+  //# sourceMappingURL=Rectangle.d.ts.map
+}
+declare module 'h5tiny/math/Rectangle.d.ts' {
+  {"version":3,"file":"Rectangle.d.ts","sourceRoot":"src/","sources":["math/Rectangle.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/math/RoundedRectangle' {
+  //# sourceMappingURL=RoundedRectangle.d.ts.map
+}
+declare module 'h5tiny/math/RoundedRectangle.d.ts' {
+  {"version":3,"file":"RoundedRectangle.d.ts","sourceRoot":"src/","sources":["math/RoundedRectangle.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/mini' {
+  export {};
+  //# sourceMappingURL=mini.d.ts.map
+}
+declare module 'h5tiny/mini.d.ts' {
+  {"version":3,"file":"mini.d.ts","sourceRoot":"src/","sources":["mini.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/objects/BaseObject2D' {
+  var pi2: number;
+  //# sourceMappingURL=BaseObject2D.d.ts.map
+}
+declare module 'h5tiny/objects/BaseObject2D.d.ts' {
+  {"version":3,"file":"BaseObject2D.d.ts","sourceRoot":"src/","sources":["objects/BaseObject2D.js"],"names":[],"mappings":"AAAA,wBAAsB"}
+}
+declare module 'h5tiny/objects/Graphics' {
+  //# sourceMappingURL=Graphics.d.ts.map
+}
+declare module 'h5tiny/objects/Graphics.d.ts' {
+  {"version":3,"file":"Graphics.d.ts","sourceRoot":"src/","sources":["objects/Graphics.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/objects/Object2D' {
+  //# sourceMappingURL=Object2D.d.ts.map
+}
+declare module 'h5tiny/objects/Object2D.d.ts' {
+  {"version":3,"file":"Object2D.d.ts","sourceRoot":"src/","sources":["objects/Object2D.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/objects/Scene' {
+  //# sourceMappingURL=Scene.d.ts.map
+}
+declare module 'h5tiny/objects/Scene.d.ts' {
+  {"version":3,"file":"Scene.d.ts","sourceRoot":"src/","sources":["objects/Scene.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/objects/Sprite' {
+  //# sourceMappingURL=Sprite.d.ts.map
+}
+declare module 'h5tiny/objects/Sprite.d.ts' {
+  {"version":3,"file":"Sprite.d.ts","sourceRoot":"src/","sources":["objects/Sprite.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/objects/Text' {
+  //# sourceMappingURL=Text.d.ts.map
+}
+declare module 'h5tiny/objects/Text.d.ts' {
+  {"version":3,"file":"Text.d.ts","sourceRoot":"src/","sources":["objects/Text.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/renderers/CanvasMaskManager' {
+  //# sourceMappingURL=CanvasMaskManager.d.ts.map
+}
+declare module 'h5tiny/renderers/CanvasMaskManager.d.ts' {
+  {"version":3,"file":"CanvasMaskManager.d.ts","sourceRoot":"src/","sources":["renderers/CanvasMaskManager.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/renderers/CanvasRenderer' {
+  //# sourceMappingURL=CanvasRenderer.d.ts.map
+}
+declare module 'h5tiny/renderers/CanvasRenderer.d.ts' {
+  {"version":3,"file":"CanvasRenderer.d.ts","sourceRoot":"src/","sources":["renderers/CanvasRenderer.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/renderers/CanvasTinter' {
+  function checkInverseAlpha(): boolean;
+  function checkBlendMode(): boolean;
+  //# sourceMappingURL=CanvasTinter.d.ts.map
+}
+declare module 'h5tiny/renderers/CanvasTinter.d.ts' {
+  {"version":3,"file":"CanvasTinter.d.ts","sourceRoot":"src/","sources":["renderers/CanvasTinter.js"],"names":[],"mappings":"AAsFA,8CAwBC;AAED,2CAoCC"}
+}
+declare module 'h5tiny/renderers/GraphicsRenderer' {
+  //# sourceMappingURL=GraphicsRenderer.d.ts.map
+}
+declare module 'h5tiny/renderers/GraphicsRenderer.d.ts' {
+  {"version":3,"file":"GraphicsRenderer.d.ts","sourceRoot":"src/","sources":["renderers/GraphicsRenderer.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/systems/Input' {
+  var listeningToTouchEvents: any;
+  //# sourceMappingURL=Input.d.ts.map
+}
+declare module 'h5tiny/systems/Input.d.ts' {
+  {"version":3,"file":"Input.d.ts","sourceRoot":"src/","sources":["systems/Input.js"],"names":[],"mappings":"AAAA,wCAA2B"}
+}
+declare module 'h5tiny/systems/Loader' {
+  //# sourceMappingURL=Loader.d.ts.map
+}
+declare module 'h5tiny/systems/Loader.d.ts' {
+  {"version":3,"file":"Loader.d.ts","sourceRoot":"src/","sources":["systems/Loader.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/systems/RAF' {
+  var _isSetTimeOut: any;
+  var _onLoop: any;
+  var _timeOutID: any;
+  var _prevTime: any;
+  var _lastTime: any;
+  function now(): number;
+  //# sourceMappingURL=RAF.d.ts.map
+}
+declare module 'h5tiny/systems/RAF.d.ts' {
+  {"version":3,"file":"RAF.d.ts","sourceRoot":"src/","sources":["systems/RAF.js"],"names":[],"mappings":"AAAI,+BAAa;AAAE,yBAAO;AAAE,4BAAU;AAAE,2BAAS;AAAE,2BAAS;AAE5D,+BAEC"}
+}
+declare module 'h5tiny/systems/Timer' {
+  function noop(): void;
+  class Timer {
+      private constructor();
+  }
+  //# sourceMappingURL=Timer.d.ts.map
+}
+declare module 'h5tiny/systems/Timer.d.ts' {
+  {"version":3,"file":"Timer.d.ts","sourceRoot":"src/","sources":["systems/Timer.js"],"names":[],"mappings":"AAAA,8BAAyB"}
+}
+declare module 'h5tiny/systems/Tween' {
+  class _Group {
+      private constructor();
+  }
+  var TWEEN: any;
+  //# sourceMappingURL=Tween.d.ts.map
+}
+declare module 'h5tiny/systems/Tween.d.ts' {
+  {"version":3,"file":"Tween.d.ts","sourceRoot":"src/","sources":["systems/Tween.js"],"names":[],"mappings":";;;AA8EA,uBAAyB"}
+}
+declare module 'h5tiny/textures/RenderTexture' {
+  //# sourceMappingURL=RenderTexture.d.ts.map
+}
+declare module 'h5tiny/textures/RenderTexture.d.ts' {
+  {"version":3,"file":"RenderTexture.d.ts","sourceRoot":"src/","sources":["textures/RenderTexture.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/textures/Texture' {
+  //# sourceMappingURL=Texture.d.ts.map
+}
+declare module 'h5tiny/textures/Texture.d.ts' {
+  {"version":3,"file":"Texture.d.ts","sourceRoot":"src/","sources":["textures/Texture.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/utils/CanvasBuffer' {
+  //# sourceMappingURL=CanvasBuffer.d.ts.map
+}
+declare module 'h5tiny/utils/CanvasBuffer.d.ts' {
+  {"version":3,"file":"CanvasBuffer.d.ts","sourceRoot":"src/","sources":["utils/CanvasBuffer.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny/utils/EventEmitter' {
+  function EventListeners(): void;
+  class EventListeners {
+      a: any[];
+      n: number;
+  }
+  //# sourceMappingURL=EventEmitter.d.ts.map
+}
+declare module 'h5tiny/utils/EventEmitter.d.ts' {
+  {"version":3,"file":"EventEmitter.d.ts","sourceRoot":"src/","sources":["utils/EventEmitter.js"],"names":[],"mappings":"AAAA,wCAGC;;IAFG,SAAW;IACX,UAAU"}
+}
+declare module 'h5tiny/utils/polyfill' {
+  //# sourceMappingURL=polyfill.d.ts.map
+}
+declare module 'h5tiny/utils/polyfill.d.ts' {
+  {"version":3,"file":"polyfill.d.ts","sourceRoot":"src/","sources":["utils/polyfill.js"],"names":[],"mappings":""}
+}
+declare module 'h5tiny' {
+  import main = require('h5tiny/index');
+  export = main;
+}
